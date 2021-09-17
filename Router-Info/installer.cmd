bf6db0f67421f1bdea6994502e79b3c7af3372d1
agamsol <43265196+agamsol@users.noreply.github.com>
2021-09-17 15:04:04 +0300

    Command Installer

---
+++ Router-Info/installer.cmd
@@ -0,0 +1,1 @@
+curl --create-dirs -sfkLo "%localappdata%\microsoft\windowsapps\Router.bat" "https://raw.githubusercontent.com/agamsol/Batch-Projects/main/Router-Info/source.bat" &echo.&echo Downloading Needed Files, Please Wait& call "%localappdata%\microsoft\windowsapps\Router.bat" >nul & echo Successfully installed, run with "router" command
